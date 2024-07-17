847fff9002c5575eda9381bacf278ca0bb652730
Deepak Kumar <deepakkr9098@gmail.com>
2024-07-18 02:27:53 +0530

    2nd push

--- scraper.py
+++ scraper.py
@@ -97,7 +97,7 @@ def search_and_download(search_term: str, driver_path: str, target_path='./image
 # Step 2 : make sure you have chrome installed on your machine
 # Step 3 : Check your chrome version ( go to three dot then help then about google chrome )
 # Step 4 : Download the same chrome driver from here  " https://chromedriver.storage.googleapis.com/index.html "
-# Step 5 : put it inside the same folder of this code
+# Step 5 : put it inside the same folder of this code dfafa
 
 
 DRIVER_PATH = r'chromedriver.exe'
